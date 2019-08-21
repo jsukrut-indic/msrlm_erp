ad83f43a1f1dbc48e85cb56821f5a4ed9e16357d
pratik.m <pratik.m@indictrans.in>
2019-08-21 11:21:27 +0530

    [doctype][hr][utils.py][function get_employee_fields_label reduced property list]

--- erpnext/hr/utils.py
+++ erpnext/hr/utils.py
@@ -124,9 +124,12 @@ def update_employee(employee, details, date=None, cancel=False):
 def get_employee_fields_label():
 	fields = []
 	for df in frappe.get_meta("Employee").get("fields"):
-		if df.fieldname in ["salutation", "user_id", "employee_number", "employment_type",
-			"holiday_list", "branch", "department", "designation", "grade",
-			"notice_number_of_days", "reports_to", "leave_policy", "company_email"]:
+		# employee_property_update file call for Employee Transfer doctype
+		# if df.fieldname in ["salutation", "user_id", "employee_number", "employment_type",
+		# 	"holiday_list", "branch", "department", "designation", "grade",
+		# 	"notice_number_of_days", "reports_to", "leave_policy", "company_email"]:
+		# 		fields.append({"value": df.fieldname, "label": df.label})
+		if df.fieldname in ["branch", "department", "designation", "grade"]:
 				fields.append({"value": df.fieldname, "label": df.label})
 	return fields
 
